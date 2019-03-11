66e120f78a15b01f3969016f21f8abc6ed2d5d27
microlith57 <microlith57@gmail.com>
2019-03-11 13:22:26 +1300

    fix(parade-ruby): Improve `look` command
    Signed-off-by: microlith57 <microlith57@gmail.com>

--- parade-ruby/lib/actions/look.rb
+++ parade-ruby/lib/actions/look.rb
@@ -4,20 +4,21 @@ module Paradise
       def act
         siblings = world.siblings_of @context[:user_vessel]
         text = ''
+
         (0..siblings.length-1).each do |index|
           vessel = siblings[index]
+
           if index == 0
-            text += vessel.pretty
+            text += "+#{vessel.pretty}+"
           elsif index < (siblings.length - 1)
-            text += ", #{vessel.pretty}"
+            text += ", +#{vessel.pretty}+"
+          elsif siblings.length <= 2
+            text += " and +#{vessel.pretty}+"
           else
-            if siblings.length < 2
-              text += " and #{vessel.pretty}"
-            else
-              text += ", and #{vessel.pretty}"
-            end
+            text += ", and +#{vessel.pretty}+"
           end
         end
+
         "You see #{text}"
       end
     end
