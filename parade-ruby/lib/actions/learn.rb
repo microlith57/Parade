3211b1f5eee4d9fc251dcf816f41aba395af764b
microlith57 <microlith57@gmail.com>
2019-03-14 10:06:51 +1300

    feat(parade-ruby): Implement the learn action
    Signed-off-by: microlith57 <microlith57@gmail.com>

---
+++ parade-ruby/lib/actions/learn.rb
@@ -0,0 +1,69 @@
+module Paradise
+  module Actions
+    class Learn < Paradise::Action
+      @doc = 'View documentation for [actions](learn about actions) and' \
+             ' [general knowlege](learn about knowledge).'
+
+      # TODO: Index of all knowledge, actions, etc
+      GENERAL_KNOWLEDGE = {
+        knowledge: 'A library of general knowledge can be accessed via' \
+                   ' +learn about+ followed by a term.',
+        actions: 'Actions are commands issued to Paradise. Action' \
+                   ' documentation can be accessed via +learn to+ followed by' \
+                   ' an action.'
+      }.freeze
+
+      def act
+        term = query_parts[1..-1].join ' '
+
+        case query_parts.first
+        when 'to'
+          learn_to term
+        when 'about'
+          learn_about term
+        end
+      end
+
+      private
+
+      def learn_to(term)
+        context = @context.dup
+        context[:query] = term.split(' ').first.downcase
+        action = Action.new(
+          @vessel, context, @server
+        ).get
+        doc = if action.doc.is_a? Array
+                action.doc[term]
+              else
+                action.doc
+              end
+        if doc.respond_to? :call
+          doc.call
+        else
+          doc
+        end
+      end
+
+      # TODO: Implement @wildcard and @:group
+      def learn_about(term)
+        if GENERAL_KNOWLEDGE.include? term.to_sym
+          learn_about_general_knowledge term.to_sym
+        else
+          raise TermNotFound, "Term '#{term}' not found."
+        end
+      end
+
+      def learn_about_general_knowledge(term)
+        knowledge = GENERAL_KNOWLEDGE[term]
+        if knowlege.respond_to :call
+          knowlege.call
+        else
+          knowledge
+        end
+      end
+
+      class TermNotFound < ParadiseException
+      end
+    end
+  end
+end
