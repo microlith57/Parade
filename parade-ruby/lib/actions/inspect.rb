fb442b6b0db8b25493269210e245afd38fac7341
microlith57 <microlith57@gmail.com>
2019-03-15 13:24:02 +1300

    feat(parade-ruby): Initial 'inspect' implementation
    Signed-off-by: microlith57 <microlith57@gmail.com>

---
+++ parade-ruby/lib/actions/inspect.rb
@@ -0,0 +1,15 @@
+module Paradise
+  module Actions
+    class Inspect < Paradise::Action
+      @doc = 'Inspect the data of a visible vessel.'
+
+      def act
+        name = query_parts
+
+        inspect_vessel = get_vessel name
+
+        "You are inspecting the +#{inspect_vessel.full}+\##{inspect_vessel.id}."
+      end
+    end
+  end
+end
